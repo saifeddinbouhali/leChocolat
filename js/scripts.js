55b7cf0e049f5ea11fe9463ded868e8e15579808
saifeddine <saifeddinbouhali@gmail.com>
2020-11-25 17:49:13 +0200

    YourName's final Bootstrap project

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ $(document).ready(function(){
         $("#mycarousel").carousel('cycle');
     });
 });
+$(document).ready(function(){
+    $('.count').prop('disabled', true);
+       $(document).on('click','.plus',function(){
+        $('.count').val(parseInt($('.count').val()) + 1 );
+    });
+    $(document).on('click','.minus',function(){
+        $('.count').val(parseInt($('.count').val()) - 1 );
+            if ($('.count').val() == 0) {
+                $('.count').val(1);
+            }
+        });
+ });
+ 
